4baec8256d414547b5609833cd2ee9097b765a61
mdnelles <mxnelles@gmail.com>
2020-04-21 12:33:43 -0400

    update

--- routes/ApiRoutes.js
+++ routes/ApiRoutes.js
@@ -12,31 +12,34 @@ const express = require("express"),
 api.use(cors());
 
 api.get("/get/:city/:geo2", (req, res) => {
+   // weatherbit working 22k return
+   // 150 calls per day
+
    let q = req.params.city + "%2C" + req.params.geo2;
-   console.log("q = " + q);
 
-   var reqApi = unirest("GET", c.global.apiUrl);
+   axios({
+      method: "GET",
+      url: "https://weatherbit-v1-mashape.p.rapidapi.com/current",
+      headers: {
+         "content-type": "application/octet-stream",
+         "x-rapidapi-host": "weatherbit-v1-mashape.p.rapidapi.com",
+         "x-rapidapi-key": "9eb7e726ebmsh421b9062a5f0fcdp1792acjsn5b07d0e7ac3f",
+      },
+      params: {
+         lang: "en",
+         lon: "-118.4068",
+         lat: "34.1139",
+      },
+   })
+      .then((response) => {
+         console.log(stringify(response));
+         res.send(stringify(response));
+      })
+      .catch((error) => {
+         console.log(error);
 
-   reqApi.query({
-      callback: "test",
-      id: c.global.apiId,
-      units: "%22metric%22 or %22imperial%22",
-      mode: "JSON",
-      q: "London%2Cuk",
-   });
-
-   reqApi.headers({
-      "x-rapidapi-host": c.global.apiHost,
-      "x-rapidapi-key": c.global.apiKey,
-   });
-
-   reqApi.end(function (response) {
-      if (response.error) {
-         res.send(response.error);
-         console.log(response.error);
-         //throw new Error(response.error);
-      }
-   });
+         res.send(error);
+      });
 });
 api.get("/geta/:city/:geo2", (req, res) => {
    let q = req.params.city + "%2C" + req.params.geo2;
@@ -47,23 +50,23 @@ api.get("/geta/:city/:geo2", (req, res) => {
       url: "https://community-open-weather-map.p.rapidapi.com/weather",
       headers: {
          "content-type": "application/octet-stream",
-         "x-rapidapi-host": c.global.apiHost,
-         "x-rapidapi-key": c.global.apiKey,
+         "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+         "x-rapidapi-key": "9eb7e726ebmsh421b9062a5f0fcdp1792acjsn5b07d0e7ac3f",
       },
       params: {
          callback: "test",
-         id: c.global.apiId,
+         id: "2172797",
          units: "%22metric%22 or %22imperial%22",
          mode: "JSON",
-         q: "London%2Cuk",
+         q: "Toronto%2CCA",
       },
    })
       .then((response) => {
-         res.send(response);
+         res.send("1");
          console.log(response);
       })
       .catch((error) => {
-         res.send(error);
+         res.send("0");
          console.log(error);
       });
 });
